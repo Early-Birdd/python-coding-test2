41ba416e7ec4cf1fd46283b1ff3053bd4cf2d8ea
EarlyBird <goqhwk1100@nate.com>
2021-12-29 23:04:20 +0900

    Commit

--- test9.py
+++ test9.py
@@ -40,45 +40,76 @@
 #
 # for i in range(1, n + 1):
 #     if distance[i] == INF:
-#         print('INFI')
+#         print("INFI")
 #     else:
 #         print(distance[i])
 
-#최단경로 1-1(다익스트라-heapq)
+#최단경로 1-2(다익스트라-heapq)
 
-import heapq
-import sys
-input = sys.stdin.readline
+# import heapq
+# import sys
+# input = sys.stdin.readline
+# INF = int(1e9)
+#
+# n, m = map(int, input().split())
+# start = int(input())
+# graph = [[] for i in range(n + 1)]
+# distance = [INF] * (n + 1)
+#
+# for _ in range(m):
+#     a, b, c = map(int, input().split())
+#     graph[a].append((b, c))
+#
+# def dstra(start):
+#     q = []
+#     #heappush(리스트, (거리, 노드)) 거리 짧은 순으로 pop
+#     heapq.heappush(q, (0, start))
+#     distance[start] = 0
+#     while q:
+#         dist, now = heapq.heappop(q)
+#         #방문한 노드라면 무시
+#         if distance[now] < dist:
+#             continue
+#         for i in graph[now]:
+#             cost = dist + i[1]
+#             if cost < distance[i[0]]:
+#                 distance[i[0]] = cost
+#                 heapq.heappush(q, (cost, i[0]))
+#
+# dstra(start)
+#
+# for i in range(1, n + 1):
+#     if distance[i] == INF:
+#         print("INFI")
+#     else:
+#         print(distance[i])
+
+#최단경로 1-3(플로이드 워셜)
+
 INF = int(1e9)
 
-n, m = map(int, input().split())
-start = int(input())
-graph = [[] for i in range(n + 1)]
-distance = [INF] * (n + 1)
+n = int(input())
+m = int(input())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
 
 for _ in range(m):
     a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-def dstra(start):
-    q = []
-    #heappush(리스트, (거리, 노드))
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-dstra(start)
+    graph[a][b] = c
 
 for i in range(1, n + 1):
-    if distance[i] == INF:
-        print('INFI')
-    else:
-        print(distance[i])
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][i] + graph[i][b])
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print("INFI", end = " ")
+        else:
+            print(graph[a][b], end = " ")
+    print()
